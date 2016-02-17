d7e370fe9c90bc0599d1b33952317864c5747239
Vignesh <Vignesh@vignesh_laptop>
2016-02-17 16:27:31 +0100

    First Commit : Creation du projet Gradle, de la classe ModelTest, de la classe Model

---
+++ src/main/java/org/gradle/model/Model.java
@@ -0,0 +1,14 @@
+package org.gradle.model;
+
+import javafx.collections.ObservableList;
+
+public class Model {
+
+	public boolean add(String s){
+		return false;
+	}
+	
+	public ObservableList<String> items(){
+		return null;
+	}
+}
